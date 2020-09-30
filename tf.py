904254a8405528718d4085d12e0f26298dfbe47a
britaliano <buono@buono.net>
2020-09-30 14:19:04 +0100

    Updated 1418

--- tf.py
+++ tf.py
@@ -17,12 +17,29 @@ df = pd.read_csv('pokemon.csv')
 # Now we need to convert a non-integer field into an integer
 df['isLegendary'] = df['isLegendary'].astype(int)
 
+# Then create dummy columns for each column in the source data set that has multiple possibble values
+# For example, Column 'sport' with values football, swimming, tennis, would create a new column for football, swimming and tennis with either 0 or 1
+# depending on the value in the source data
 def dummy_creation(df, dummy_categories):
+    # For each column passed when the function is called
     for i in dummy_categories:
+
+        # Get the 'dummies' (possible values) for each column
         df_dummy = pd.get_dummies(df[i])
+
+        # Add a new column for each dummy column to the source data
         df = pd.concat([df,df_dummy],axis=1)
+
+        # Then drop the column from the source data as we do not need it
         df = df.drop(i, axis=1)
     return(df)
 
 
-    
+# Call the above function and pass the columns we want to dummify
+df = dummy_creation(df, ['Egg_Group_1', 'Body_Style', 'Color','Type_1', 'Type_2'])
+print (df)
+
+full_result_dataframe = pd.DataFrame(df)
+print(full_result_dataframe)
+# Print the table to an Excel file
+full_result_dataframe.to_excel("PokemonData.xlsx")
