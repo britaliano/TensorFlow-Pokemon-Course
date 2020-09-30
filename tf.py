04f6bfe0bf28d88e3d86cf1fae28c34857f694e3
britaliano <buono@buono.net>
2020-09-30 10:59:32 +0100

    Making progreaa

---
+++ tf.py
@@ -0,0 +1,19 @@
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn import preprocessing
+
+
+df = pd.read_csv('pokemon.csv')
+
+# You can print out all the column names
+# print (df.columns)
+
+# Or you can choose the ones you want
+# df = df[['isLegendary','Generation', 'Type_1', 'Type_2', 'HP', 'Attack', 'Defense', 'Sp_Atk', 'Sp_Def', 'Speed','Color','Egg_Group_1','Height_m','Weight_kg','Body_Style']]
+
+# Now we need to convert a non-integer field into an integer
+df['isLegendary'] = df['isLegendary'].astype(int)
+
