f550998e740f25edbb205903b9eb81a5352862a3
britaliano <buono@buono.net>
2020-09-30 13:35:22 +0100

    20200930_1335

--- tf.py
+++ tf.py
@@ -17,3 +17,12 @@ df = pd.read_csv('pokemon.csv')
 # Now we need to convert a non-integer field into an integer
 df['isLegendary'] = df['isLegendary'].astype(int)
 
+def dummy_creation(df, dummy_categories):
+    for i in dummy_categories:
+        df_dummy = pd.get_dummies(df[i])
+        df = pd.concat([df,df_dummy],axis=1)
+        df = df.drop(i, axis=1)
+    return(df)
+
+
+    
